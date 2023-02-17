9504683c71a931cbab3c1bc06da60e5f83f5a7f7
From_Work <weymar87@gmail.com>
2023-02-17 16:50:42 +0500

    Initial commit

---
+++ src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
