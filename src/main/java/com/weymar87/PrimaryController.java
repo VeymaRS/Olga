af5dc2d8ad6c583842e3e7171207909f5029804a
From_Work <weymar87@gmail.com>
2023-02-22 16:31:13 +0500

    Ver.2 from 2023.02.22

--- src/main/java/com/weymar87/PrimaryController.java
+++ src/main/java/com/weymar87/PrimaryController.java
@@ -2,9 +2,12 @@ package com.weymar87;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
+    @FXML
+    private TextField
 
 
     @FXML
