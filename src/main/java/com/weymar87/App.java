74091bae77ea772f15dd522b9c60f2031dfa3af3
VeymaR (notepad) <weymar87@gmail.com>
2023-02-25 18:50:15 +0500

    Ver.1 from 2023.02.25

--- src/main/java/com/weymar87/App.java
+++ src/main/java/com/weymar87/App.java
@@ -1,9 +1,12 @@
 package com.weymar87;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,21 +18,51 @@ public class App extends Application {
 
     private static Scene scene;
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    PrimaryController primaryController;
+    private ObservableList<Integer> listPile = FXCollections.observableArrayList();
+
+    public App() {
+        listPile.add(89);
+        listPile.add(114);
+        listPile.add(159);
+        listPile.add(219);
+        listPile.add(273);
+    }
+
+    public ObservableList<Integer> getListPile() {
+        return listPile;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"));
-        stage.setScene(scene);
-        stage.show();
+        this.primaryStage = stage;
+        this.primaryStage.setTitle("SunnyFroze");
+        initRootLayout();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    public void initRootLayout() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+            rootLayout = (BorderPane) fxmlLoader.load();
+            scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryController = fxmlLoader.getController();
+            primaryController.setApp(this);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+//    static void setRoot(String fxml) throws IOException {
+//        scene.setRoot(loadFXML(fxml));
+//    }
+//
+//    private static Parent loadFXML(String fxml) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+//        return fxmlLoader.load();
+//    }
 
     public static void main(String[] args) {
         launch();
--- src/main/java/com/weymar87/PrimaryController.java
+++ src/main/java/com/weymar87/PrimaryController.java
@@ -1,17 +1,29 @@
 package com.weymar87;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class PrimaryController {
+    App app;
 
     @FXML
-    private TextField
-
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ComboBox<Integer> pileDiameter;
+    public void setApp(App app) {
+        this.app = app;
+        pileDiameter = new ComboBox<>(app.getListPile());
     }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+
+//    @FXML
+//    private void switchToSecondary() throws IOException {
+//        App.setRoot("secondary");
+//    }
 }
